a005540b28ffacf4b08874e61c216d291fff1eba
Peter Claver <pitamcclav@gmail.com>
2024-06-06 13:01:23 +0300

    Initial commit

---
+++ CRUD/Controllers/HelloWorldController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CRUD.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Welcome(string name, int age = 4)
+        {
+            ViewData["Message"] = "Hello" + name;
+            ViewData["Age"] = age;
+            return View();
+        }
+
+    }
+}
