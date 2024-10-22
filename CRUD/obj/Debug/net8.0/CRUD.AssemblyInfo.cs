7dcd6b2b595cac8f4bbccd5dd34bf2ebafe3f7d6
ssekitooleko.peter <pitamcclav@gmail.com>
2024-10-22 14:28:18 +0300

    I really have no idea

--- CRUD/obj/Debug/net8.0/CRUD.AssemblyInfo.cs
+++ CRUD/obj/Debug/net8.0/CRUD.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CRUD")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a005540b28ffacf4b08874e61c216d291fff1eba")]
 [assembly: System.Reflection.AssemblyProductAttribute("CRUD")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CRUD")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
